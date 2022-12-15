e844dd772a95d6d4d79189fd436ff8ae85245d90
shinhee0401 <112532534+shinhee0401@users.noreply.github.com>
2022-12-15 20:25:20 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+//menu
+document.querySelectorAll(".nav ul li a").forEach(li => {
+  li.addEventListener("click", e => {
+      e.preventDefault();
+      document.querySelector(li.getAttribute("href")).scrollIntoView({
+          behavior: "smooth"
+      })
+  })
+})
+
+$('.ham').click(function () {
+  $('#mNav').toggleClass('active')
+});
+$('.mNav_cont ul li').click(function () {
+  $('#mNav').removeClass('active')
+});
+
+
+
+//section1 
+$(".split").each(function () {
+  let text = $(this).text();
+  let split = text.split('').join("</span><span aria-hidden='true'>");
+  split = "<span aria-hidden='true'>" + split + "</span>";
+
+  $(this).html(split).attr("aria-label", text);
+});
+
+setTimeout(function () {
+  gsap.to(".split span", {
+      duration: 0.4,
+      opacity: 1,
+      y: 0,
+      stagger: 0.1
+  })
+}, 2000)
---
+++ assets/js/project.js
@@ -0,0 +1,38 @@
+const xhr = new XMLHttpRequest();
+//XMLHttpRequest() - 외부 데이터를 불러들이는 객체
+
+xhr.open('get','project.json',true);   //요청을 준비하다
+xhr.send(null);	       //요청을 전송한다
+
+xhr.onload = function(){
+  
+   if(xhr.status == 200){ //서버 응답이 정상이라면 (Http status code) 
+    respObj = JSON.parse(xhr.responseText);
+
+    let newContent = '';
+    for(let i = 0; i < respObj.project.length; i++){
+      newContent += `<div class="box">`;
+      newContent += `<div class="click">Click</div><img src= "${respObj.project[i].url}" alt="${respObj.project[i].alt}">
+      `;
+      newContent += `</div>`;
+    }
+
+
+    let newModal = '';
+    for(let i = 0; i < respObj.explanation.length; i++){
+      newModal += `<div class="modal-content">`;
+      newModal += `<span class="close">&times;</span>`;
+      newModal += `<img src= "${respObj.explanation[i].url}" alt="${respObj.explanation[i].alt}">`;
+      newModal += `<h3><span>title</span> ${respObj.explanation[i].title}</h3>`;
+      newModal += `<p><span>skill</span> ${respObj.explanation[i].skill}</p>`;
+      newModal += `<div class="click_button"><button class="view"><a href="${respObj.explanation[i].view}" target="_blank">view site</a></button>
+          <button class="code">view code</button>
+        </div>`;
+      newModal += `</div>`;
+    }
+
+
+    document.getElementById('project_img').innerHTML = newContent;
+    document.getElementById('modal').innerHTML = newModal;
+  }
+}  
